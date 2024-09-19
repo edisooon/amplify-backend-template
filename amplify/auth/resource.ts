cb08d8fc35941c29669467f3422131167090ed67
Edison Zhang <edisonzz@amazon.com>
2024-09-19 12:55:04 -0700

    only custom

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,21 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from "@aws-amplify/backend"
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
-});
+    loginWith: {
+        // this configures a required "email" attribute
+        email: true,
+    },
+
+    userAttributes: {
+        "custom:display_name": {
+            dataType: "String",
+            mutable: true,
+            maxLen: 16,
+            minLen: 1
+        },
+        // birthdate: {
+        //     mutable: true,
+        //     required: false,
+        // },
+    }
+})
